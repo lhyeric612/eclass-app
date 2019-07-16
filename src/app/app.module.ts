f5ef7584891b01e5f8261beed581d1d08689f2f3
Eric Lai <eric.lai@matizon.com>
2019-07-16 14:59:38 +0800

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+    { path: '', component: LoginComponent },
+    // { path: 'crisis-center', component: CrisisListComponent },
+    // { path: 'hero/:id',      component: HeroDetailComponent },
+    // {
+    //     path: 'heroes',
+    //     component: HeroListComponent,
+    //     data: { title: 'Heroes List' }
+    // },
+    // { path: '',
+    //     redirectTo: '/heroes',
+    //     pathMatch: 'full'
+    // },
+    // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
